ebd3a5914146c436025c151307db4977aa5b36d0
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2025-04-29 22:53:09 +0700

    [Update] - Project Setup

---
+++ src/main/java/com/fadhiilabiyyi/license3j/License3jApplication.java
@@ -0,0 +1,13 @@
+package com.fadhiilabiyyi.license3j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class License3jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(License3jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fadhiilabiyyi/license3j/License3jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadhiilabiyyi.license3j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class License3jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
